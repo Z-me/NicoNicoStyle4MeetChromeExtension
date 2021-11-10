061f22e5f5f607709e456a4ea6f8337655cd4840
Hajime <b1011177@hotmail.co.jp>
2021-11-10 10:39:44 +0900

    update class name

--- script/const.js
+++ script/const.js
@@ -1,11 +1,11 @@
 const CLASS_OBJ = {
-  thread: 'z38b6 CnDs7d hPqowe',
-  messages: 'oIy2qc'
+  thread: 'z38b6',
+  messages: 'oIy2qc',
 }
 const INPUT_AREA = 'textarea[name="chatTextInput"]'
 const OBSERVE_CONFIG = {
   attributes: true,
   subtree: true,
   childList: true,
-  characterData: true
+  characterData: true,
 }
